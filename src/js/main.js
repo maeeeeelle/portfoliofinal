df9dd7477b4fff333badb901e5f2982bcf9f0bce
maeeeeelle <maelle.ecoffey@studentfr.ch>
2025-04-16 17:04:41 +0200

    finaaaalecommit

--- src/js/main.js
+++ src/js/main.js
@@ -2,13 +2,6 @@ import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 
-// ScrollTrigger.create({
-//   trigger: ".image-gsap",
-//   start: "top top",
-//   pin: true,
-//   pinSpacing: false,
-// });
-
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
@@ -20,26 +13,6 @@ images.forEach((image) => {
     trigger: image,
     start: "top top",
     pin: true,
-    pinSpacing: false, // change en true si tu veux un espacement entre les pins
+    pinSpacing: false,
   });
 });
-
-// var allProject = document.querySelectorAll(".image-container-alone");
-
-// var toggleMenu = function toggleMenu(pop) {
-//   pop.classList.toggle("is-active");
-//   document.body.classList.toggle("project-no-scroll");
-//   document.querySelector("project-pop.is-active").classList.toggle("active");
-// };
-
-// allProject.forEach(function (imageContainerAlone) {
-//   var pop = imageContainerAlone.querySelector(".project-pop");
-//   var img = imageContainerAlone.querySelector(".image-pop");
-//   var cross = imageContainerAlone.querySelector(".project-cross");
-//   img.addEventListener("click", () => {
-//     toggleMenu(pop);
-//   });
-//   cross.addEventListener("click", () => {
-//     toggleMenu(pop);
-//   });
-// });
